510630b64ddd15e21dd8eb7c1740add029bf44d2
Liulele <liulele@qinengda.cn>
2024-04-23 17:10:55 +0800

    添加项目文件。

---
+++ CompleteGUI/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CompleteGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CancellationTokenSource cancellationTokenSource;
+        CancellationToken cancellationToken;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnProcess_Click(object sender, RoutedEventArgs e)
+        {
+            btnProcess.IsEnabled = false;//首先置灰按钮
+            
+            //下面要处理ProgressBar的效果了
+            cancellationTokenSource = new CancellationTokenSource();
+            cancellationToken = cancellationTokenSource.Token;
+            int completedPercent = 0;
+            //每隔0.5秒 进度条多完成10%
+            for (int i = 0; i < 10; i++)
+            {
+                if (cancellationToken.IsCancellationRequested) break;
+                try
+                {
+                    //下面这段是异步执行的
+                    await Task.Delay(500, cancellationToken);
+                    completedPercent = (i + 1) * 10;
+                }
+                catch (TaskCanceledException ex)
+                {
+                    completedPercent = i * 10;
+                }
+                progressBar.Value = completedPercent;
+            }
+
+            //如果点击了Cancel按钮，上面的异步操作提前结束；或者异步操作正常结束
+            string message = cancellationToken.IsCancellationRequested ? string.Format($"Process was cancelled at {completedPercent}%.") : "Process completed normally.";
+            MessageBox.Show(message, "Completion Status");
+
+            progressBar.Value = 0;
+            btnProcess.IsEnabled = true;
+            btnCancel.IsEnabled = true;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            if(!btnProcess.IsEnabled)
+            {
+                btnCancel.IsEnabled = false;
+                cancellationTokenSource.Cancel();
+            }
+        }
+    }
+}
---
+++ GUI_Asychronous/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI_Asychronous
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //使用Lambda表达式创建一个事件
+            startWorkButton.Click += async (sender, e) =>
+            {
+                SetGuiValues(false, "Work Started");//设置窗口表现
+                await DoSomeWork();
+                SetGuiValues(true, "Work Finished");
+            };
+        }
+
+        private Task DoSomeWork()
+        {
+            return Task.Delay(2500);
+        }
+
+        private void SetGuiValues(bool v1, string v2)
+        {
+            startWorkButton.IsEnabled = v1;
+            workStartedTextBlock.Text = v2;
+        }
+
+        /*private async void btnDoStuff_Click(object sender, RoutedEventArgs e)
+        {
+            btnDoStuff.IsEnabled = false;//置灰这个button
+            lblStatus.Content = "Doing Stuff";//改变按钮上方的文字
+            await Task.Delay(4000);//阻塞当前线程4秒钟，模拟处理别的任务
+            lblStatus.Content = "Not Doing Anything";
+            btnDoStuff.IsEnabled = true;
+        }*/
+
+
+    }
+}
